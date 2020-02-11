a8d53917a2952e2ac1d45c2a8c7790d31fdf5edf
lukes.code <lukesterry@gmail.com>
2020-02-11 18:47:35 +0000

    login button fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,14 +10,13 @@ class Landing extends React.Component {
                     <p id="sub-text">easily navigate work safe music</p>
                 </div>
                 <div className="landing-button">
-                <button
-                    className="def-btn"
-                    // onClick={this.props.login}
-                >
-                    <a href="https://accounts.spotify.com/authorize?client_id=32eceb9f568649ada17a620892f797e0&redirect_uri=https://workify.netlify.com/callback/&scope=user-top-read user-read-private user-read-email user-read-recently-played user-follow-read&response_type=token&state=123">
+                <a id="login" href="https://accounts.spotify.com/authorize?client_id=32eceb9f568649ada17a620892f797e0&redirect_uri=https://workify.netlify.com/callback/&scope=user-top-read user-read-private user-read-email user-read-recently-played user-follow-read&response_type=token&state=123">
+                    <button
+                        className="def-btn"
+                    >
                     Log in to Spotify
-                    </a>
-                </button>
+                    </button>
+                </a>
                 </div>
             </div>
         );
