15b5a7abb656d90f83613ab1498d2e26948fd0a2
Luke Burton <lukesterry@gmail.com>
2022-07-04 22:53:49 +0100

    live redirect uri

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,7 @@ export const Landing = () => {
                 <p id="sub-text">easily navigate work safe music</p>
             </div>
             <div className="landing-button">
-            <a id="login" href="https://accounts.spotify.com/authorize?client_id=32eceb9f568649ada17a620892f797e0&redirect_uri=http://localhost:3000/callback&response_type=token&state=123">
+            <a id="login" href="https://accounts.spotify.com/authorize?client_id=32eceb9f568649ada17a620892f797e0&redirect_uri=https://workify.netlify.com/callback/&scope=user-top-read user-read-private user-read-email user-read-recently-played user-follow-read&response_type=token&state=123">
                 <button
                     className="def-btn"
                 >
